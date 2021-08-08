51be8a1271f10813762ee449927028a9dee4c566
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2021-08-08 02:23:12 -0300

    add the oo folder

---
+++ OO/Inheritance.js
@@ -0,0 +1,4 @@
+//                                          Inheritance in javascript
+
+/*In javascript, inheritance is based on prototype, that is, i can instantiate objects
+having has inheritance the prototype (functions and/or her variables) of the contructor function : */
